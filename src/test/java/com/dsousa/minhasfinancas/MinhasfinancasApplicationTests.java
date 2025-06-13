a2fe9b04cc6b576fc159e28d59162e0a24fefb06
Ruan Dias <mra_rdias@veloecorp.com.br>
2025-06-13 17:00:12 -0300

    first commit

---
+++ src/test/java/com/dsousa/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dsousa.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+public class MinhasfinancasApplicationTests {
+
+}
